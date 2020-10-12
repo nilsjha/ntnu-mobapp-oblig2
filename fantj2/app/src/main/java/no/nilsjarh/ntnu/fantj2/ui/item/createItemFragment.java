83ee73739b4163884b11bee81faa317d4f1a5d9d
Nils Jarle Haugen <nils+git@gaupne.net>
2020-10-13 00:47:36 +0200

    add item fragment design

--- fantj2/app/src/main/java/no/nilsjarh/ntnu/fantj2/ui/item/createItemFragment.java
+++ fantj2/app/src/main/java/no/nilsjarh/ntnu/fantj2/ui/item/createItemFragment.java
@@ -61,6 +61,6 @@ public class createItemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reciept, container, false);
+        return inflater.inflate(R.layout.fragment_create_item, container, false);
     }
 }
