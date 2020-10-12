162ec2d3846ab3e6fda4e6066fea30af3e5e966f
Nils Jarle Haugen <nils+git@gaupne.net>
2020-10-13 01:00:03 +0200

    git version 2.28.0 backup/3/JAVA:MAIN_ACTIVITY/XML:FRAGMENT_ITEM/MOBILE_NAVIGATION

--- fantj2/app/src/main/java/no/nilsjarh/ntnu/fantj2/MainActivity.java
+++ fantj2/app/src/main/java/no/nilsjarh/ntnu/fantj2/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_market, R.id.nav_item, R.id.nav_login)
+                R.id.nav_home, R.id.nav_market, R.id.nav_item, R.id.nav_login, R.id.nav_create)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
