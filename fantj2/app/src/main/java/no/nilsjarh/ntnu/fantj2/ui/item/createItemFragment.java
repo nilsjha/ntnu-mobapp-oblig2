a3b90d3d7e1ba090eeb0d3eb89baf0ad52685c04
Nils Jarle Haugen <nils+git@gaupne.net>
2020-10-13 17:10:11 +0200

    Implemented create item

--- fantj2/app/src/main/java/no/nilsjarh/ntnu/fantj2/ui/item/createItemFragment.java
+++ fantj2/app/src/main/java/no/nilsjarh/ntnu/fantj2/ui/item/createItemFragment.java
@@ -1,5 +1,6 @@
 package no.nilsjarh.ntnu.fantj2.ui.item;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -12,13 +13,17 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.snackbar.SnackbarContentLayout;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 import no.nilsjarh.ntnu.fantj2.R;
 import no.nilsjarh.ntnu.fantj2.Result;
@@ -67,36 +72,72 @@ public class createItemFragment extends Fragment {
             }
         });
 
+
         acceptBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar status = Snackbar.make(root,"Please hold on while creating the item...", Snackbar.LENGTH_INDEFINITE);
-                status.show();
-                itemViewModel.createNewItem(null, null, createdItem -> {
-                    status.setDuration(BaseTransientBottomBar.LENGTH_LONG);
-                    if (createdItem instanceof Result.Success) {
-                        // Item created, view model updated with new item
-                        status.setTextColor(Color.GREEN).setText("Success").show();
-                        navController.navigate(R.id.action_nav_create_to_nav_item);
-                    } else {
-                        Exception error = ((Result.Error) createdItem).getError();
-                        if (error == null) {
-                            // RETURNED NULL BECUASE SERVER RJECTED OUR REQUEST
-                            status.setTextColor(Color.RED).setText("Ensure that the title and price").show();
-                        } else {
-                            // CLIENT SIDE ERROR
-                            if (error instanceof IOException) {
-                                status.setTextColor(Color.YELLOW).setText("Network error, please chack your connected").show();
-                            } else {
-                                status.setTextColor(Color.RED).setText("Unable to create item, please restart the app").show();
-                            }
+                Snackbar status = Snackbar.make(root,"",Snackbar.LENGTH_LONG);
+                String titleString = title.getText().toString();
+                String priceString = price.getText().toString();
+                if (validateInput(titleString,priceString)) {
+                    // VALID INPUT
+                    status.setText("Please hold on while creating the item....").setDuration(Snackbar.LENGTH_INDEFINITE).show();
 
+                    itemViewModel.createNewItem(title.getText().toString(), new BigDecimal(price.getText().toString()), createdItem -> {
+                        status.setDuration(BaseTransientBottomBar.LENGTH_LONG);
+                        if (createdItem instanceof Result.Success) {
+                            // Item created, view model updated with new item
+                            status.setDuration(BaseTransientBottomBar.LENGTH_SHORT);
+                            status.setTextColor(Color.GREEN).setText("Success").show();
+                            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                            inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                            navController.navigate(R.id.action_nav_create_to_nav_item);
+                        } else {
+                            Exception error = ((Result.Error) createdItem).getError();
+                            if (error == null) {
+                                // RETURNED NULL BECUASE SERVER RJECTED OUR REQUEST
+                                status.setTextColor(Color.RED).setText("Ensure that the item got a title and price").show();
+                            } else {
+                                // CLIENT SIDE ERROR
+                                if (error instanceof IOException) {
+                                    status.setTextColor(Color.YELLOW).setText("Network error, please chack the conntection").show();
+                                } else {
+                                    status.setTextColor(Color.RED).setText("Unable to create item, please restart the app").show();
+                                }
+
+                            }
                         }
-                    }
-                });
+                    });
+                } else {
+                    //INVALID INPUT
+                    status.setTextColor(Color.RED).setText("Title or price is invalid");
+                    status.setDuration(BaseTransientBottomBar.LENGTH_LONG).show();
+                }
+
             }
         });
 
         return root;
     }
+
+    /**
+     * Validate data input from user in the app
+     * @param title string title, checked if empty
+     * @param price string price, checked if empty and valid number
+     * @return true if all checks are valid and may be accepted by server
+     */
+    private Boolean validateInput( String title, String price ) {
+        if ((title.isEmpty()) || (price.isEmpty())) {
+            return false;
+        } else {
+            try {
+                BigDecimal parseTest = new BigDecimal(price);
+                return true;
+            } catch (NumberFormatException ne) {
+               return false;
+            }
+        }
+    }
+
 }
